e14cfc913a888241acd3d96d8d887bdacf356ef2
francisca <franskyl@yahoo.co.uk>
2024-02-29 10:09:34 +0100

    first commit - commiting all file to the repository

---
+++ app.js
@@ -0,0 +1,72 @@
+// varaiables
+
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const overOverlay = document.querySelector(".cart-overlay");
+const clearDOM = document.querySelector(".cart");
+const clearItems = document.querySelector(".cart-items");
+const clearTotal = document.querySelector(".cart-total");
+const clearContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
+
+// cart
+
+let cart = [];
+
+// getting products
+
+class Products{
+ async getProducts(){
+  try {
+ let result = await fetch('products.json');
+ let data = await result.json();
+
+ let products = data.items;
+ products = products.map(item =>{
+  const {title,price} =item.fields;
+  console.log(item)
+  const {id} = item.sys;
+  const image = item.fields.image.fields.file.src;
+  console.log(image)
+  return {title,price,id,image}
+  
+ })
+ return products
+ } catch (error){
+  console.log(error);
+ }
+ }
+}
+// display products
+class UI{
+ displayProducts(products){
+let result = "";
+products.forEach(product => {
+ result +=`
+ <article class="product">
+    <div class="img-container">
+     <img src=${product.image}" alt="product" class="Product-img"/>
+     <button class="bag-btn" data-id=${product.id}>
+      <i class="fas fa-shopping-cart"></i>
+       add to bag
+      </button>
+    </div>
+    <h3>${product.title}</h3>
+    <h4>$ ${product.price}</h4>
+   </article>
+ `;
+});
+productsDOM.innerHTML = result;
+ }
+}
+// Local storage
+class storage{
+}
+document.addEventListener("DOMContentLoaded",()=>{
+ const ui = new UI();
+ const products = new Products();
+
+// get all products
+products.getProducts().then(products => ui.displayProducts(products));
+});
